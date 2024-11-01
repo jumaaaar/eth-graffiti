63c2909433c0a5383614deb6239d0f85b2e01207
Jomar <jomarcotejo@gmail.com>
2024-11-01 23:54:47 +1100

    added config

---
+++ config.lua
@@ -0,0 +1,65 @@
+Config = {}
+
+--- GRAFITTI CONFIG
+
+Config.Graffitis = {}
+
+Config.BlacklistedZones = {
+    {coords = vector3(455.81, -997.04, 43.69), radius = 200.0}, -- Police
+    {coords = vector3(324.76, -585.72, 59.15), radius = 300.0}, -- Hospital
+    {coords = vector3(-376.73, -119.47, 40.73), radius = 400.0},
+    {coords = vector3(433.29, 8.64, 91.94), radius = 400.0}, -- Mechanic
+}
+
+
+
+Config.ProtectionArea = {
+
+    --- TWENTT FOUR SEVENS ---
+    {coords = vector3(373.8, 325.8, 103.5) , isTaken = false},
+    {coords = vector3(2557.4, 382.2, 108.6) , isTaken = false},
+    {coords = vector3(-3038.9, 585.9, 7.9) , isTaken = false},
+    {coords = vector3(-3241.9, 1001.4, 12.8) , isTaken = false},
+    {coords = vector3(547.4, 2671.7, 42.1) , isTaken = false},
+    {coords = vector3(1961.4, 3740.6, 32.3) , isTaken = false},
+    {coords = vector3(2678.9, 3280.6, 55.2) , isTaken = false},
+    {coords = vector3(1729.2, 6414.1, 35.0) , isTaken = false},
+    {coords = vector3(24.21, -1346.79, 29.5) , isTaken = false},
+
+
+    --- LIQUOR SHOPS ---
+    {coords = vector3(1137.8, -982.2, 46.4) , isTaken = false},
+    {coords = vector3(-1224.9, -906.9, 12.3), isTaken = false},
+    {coords = vector3(-1489.5, -379.1, 40.1), isTaken = false},
+    {coords = vector3(-2970.2, 390.9, 15.0) , isTaken = false},
+    {coords = vector3(1168.0, 2708.9, 38.1) , isTaken = false},
+    {coords = vector3(1394.5, 3604.6,  34.9) , isTaken = false},
+	
+    ---  LTD Gasoline ----
+
+    {coords = vector3(-48.5,  -1757.5, 29.4), isTaken = false},
+    {coords = vector3(1163.3, -323.8, 69.2), isTaken = false},
+    {coords = vector3(-1487.5, -379.1, 40.1), isTaken = false},
+    {coords = vector3(-707.5, -914.2, 19.2), isTaken = false},
+    {coords = vector3(-1820.5, 792.5, 138.1) , isTaken = false},
+    {coords = vector3(1698.3, 4924.4, 42.0) , isTaken = false},
+	
+    --- Clotheshop -----
+
+    {coords = vector3(72.3, -1399.1, 29.4), isTaken = false},
+    {coords = vector3(-703.8, -152.3, 37.4), isTaken = false},
+    {coords = vector3(-167.9, -299.0, 39.7), isTaken = false},
+    {coords = vector3(428.7, -800.1, 29.5), isTaken = false},
+    {coords = vector3(-829.4, -1073.7, 11.3) , isTaken = false},
+    {coords = vector3(-1447.8, -242.5, 49.8) , isTaken = false},
+    {coords = vector3(11.6, 6514.2, 31.9), isTaken = false},
+    {coords = vector3(123.6, -219.4, 54.6), isTaken = false},
+    {coords = vector3(1696.3, 4829.3, 42.1), isTaken = false},
+    {coords = vector3(618.1, 2759.6, 42.1), isTaken = false},
+    {coords = vector3(1190.6, 2713.4, 38.2) , isTaken = false},
+    {coords = vector3(-1193.4, -772.3, 17.3) , isTaken = false},
+    {coords = vector3(-3172.5, 1048.1, 20.9), isTaken = false},
+    {coords = vector3(-1108.4, 2708.9, 19.1), isTaken = false},
+
+}
+
